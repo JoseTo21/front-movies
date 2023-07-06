027bdfe4a2a14318928c39db6f0dcc552245511b
JoseTo21 <toro.jose.8e@gmail.com>
2023-07-06 17:26:50 -0400

    finishedact

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
-import './App.css'
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import { Home, Directors, Actors, MovieDetail } from './pages'
-import { LoadingScreen, NavBar, Notification } from './components'
-import { useEffect } from 'react'
-import { getGenresThunk } from './store/slices/genres.slice'
-import { useDispatch, useSelector } from 'react-redux'
-import { getActorsThunk } from './store/slices/actors.slice'
-import { getDirectorsThunk } from './store/slices/directors.slice'
-import { getMoviesThunk } from './store/slices/movies.slice'
-import MovieForm from './pages/MovieForm'
+import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home, Directors, Actors, MovieDetail } from "./pages";
+import { LoadingScreen, NavBar, Notification } from "./components";
+import { useEffect } from "react";
+import { getGenresThunk } from "./store/slices/genres.slice";
+import { useDispatch, useSelector } from "react-redux";
+import { getActorsThunk } from "./store/slices/actors.slice";
+import { getDirectorsThunk } from "./store/slices/directors.slice";
+import { getMoviesThunk } from "./store/slices/movies.slice";
+import MovieForm from "./pages/MovieForm";
 
 function App() {
-
   const dispatch = useDispatch();
-  const isLoading = useSelector(state => state.app.isLoading);
+  const isLoading = useSelector((state) => state.app.isLoading);
 
   useEffect(() => {
     dispatch(getGenresThunk());
     dispatch(getActorsThunk());
     dispatch(getDirectorsThunk());
     dispatch(getMoviesThunk());
-  }, [])
+  }, []);
 
   return (
     <HashRouter>
-      <div className="educational-purposes" >
-            Front-end made by Academlo instructors for educational purposes
-        </div>
       <NavBar />
       <Notification />
-      { isLoading && <LoadingScreen /> }
-      <Container className='my-5'>
+      {isLoading && <LoadingScreen />}
+      <Container className="my-5">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/actors" element={<Actors />} />
@@ -42,7 +38,7 @@ function App() {
         </Routes>
       </Container>
     </HashRouter>
-  )
+  );
 }
 
-export default App
+export default App;
